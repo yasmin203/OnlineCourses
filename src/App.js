53c5d0055dd7897b4ec291c0015c338145acc557
yasmin203 <anayasoo2031995@gmail.com>
2023-10-27 17:38:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
       <Header/>
-        <Routes basename={"OnlineCourses"} >
+        <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/courses' element={<Courses />} />
